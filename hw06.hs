50efa16041458589cf16a78f33f2f30197a1e8f9
isaev <valery.isaev@gmail.com>
2015-03-26 02:22:42 +0300

    A message

--- hw06.hs
+++ hw06.hs
@@ -2,6 +2,21 @@ import qualified Data.Map as M
 import Prelude hiding (lookup)
 import Test.HUnit
 
+
+
+
+{-
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Не надо ничего комментировать, оно же не мешается.
+Иначе тесты нельзя запустить.
+
+И не нужно менять тесты, к тому же на неправильные. Зачем это вообще?
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-}
+
+
+
+
 ------------------------------------------------------------------------------
 -- 1. Реализуйте функции для работы с комплекснми числами.
 
@@ -36,7 +51,7 @@ conj a = Complex (real a) ((im a) * (-1))
 testsComplex =
     [ i *. i ~?= fromDouble (-1)
     , fromDouble 3 +. i ~?= Complex 3 1
-    , fromDouble 3 *. i ~?= Complex 0 2
+    , fromDouble 3 *. i ~?= Complex 0 3 -- Почему тут двойка стояла?
     , (fromDouble 3 +. fromDouble 4 *. i) *. (fromDouble 4 +. fromDouble 3 *. i) ~?= fromDouble 25 *. i
     , conj (fromDouble 3 +. fromDouble 4 *. i) ~?= fromDouble 3 -. fromDouble 4 *. i
     , fromDouble 2 /. (fromDouble 1 +. i) ~?= fromDouble 1 -. i
@@ -119,62 +134,62 @@ width t = width' $ children t
 --------------------------------------------------------------------------------
 ---- 3
 
---data Value = I Int | B Bool deriving (Eq, Show)
---data BinOp = Plus | Mul | Minus | Less | Greater | Equals
---data UnOp = Neg | Not
---data Expr = BinOp BinOp Expr Expr | UnOp UnOp Expr | Const Value | If Expr Expr Expr | Var String
---data Statement = Assign String Expr | While Expr Statement | Compound [Statement]
+data Value = I Int | B Bool deriving (Eq, Show)
+data BinOp = Plus | Mul | Minus | Less | Greater | Equals
+data UnOp = Neg | Not
+data Expr = BinOp BinOp Expr Expr | UnOp UnOp Expr | Const Value | If Expr Expr Expr | Var String
+data Statement = Assign String Expr | While Expr Statement | Compound [Statement]
 
---infixr 0 @=
---(@=) = Assign
---(.+) = BinOp Plus
---(.-) = BinOp Minus
---(.*) = BinOp Mul
---(.<) = BinOp Less
---int = Const . I
---bool = Const . B
---neg = UnOp Neg
+infixr 0 @=
+(@=) = Assign
+(.+) = BinOp Plus
+(.-) = BinOp Minus
+(.*) = BinOp Mul
+(.<) = BinOp Less
+int = Const . I
+bool = Const . B
+neg = UnOp Neg
 
---type Error = String
+type Error = String
 
 ---- evalExpr m e интерпретирует выражение e, в m передается значение переменных.
 ---- evalExpr возвращает либо успешно вычисленный результат, либо список ошибок.
 ---- Ошибки бывают двух видов: необъявленная переменная и несоответствие типов.
 ---- Возвращается список ошибок, т.к. выражение может содержать больше одной ошибки.
---evalExpr :: M.Map String Value -> Expr -> Either [Error] Value
---evalExpr = undefined
+evalExpr :: M.Map String Value -> Expr -> Either [Error] Value
+evalExpr = undefined
 
 ---- evalStatement принимает текущее значение переменных и statement и возвращает новое значение переменных после его выполнения.
---evalStatement :: M.Map String Value -> Statement -> Either [Error] (M.Map String Value)
---evalStatement = undefined
+evalStatement :: M.Map String Value -> Statement -> Either [Error] (M.Map String Value)
+evalStatement = undefined
 
 ---- tests
 
---max' x y = If (x .< y) y x
---expr1 = Var "x" .+ int 3
---expr2 = If (Var "x") (Var "y" .- int 3) (int 2)
---stat1 = Compound
---    [ "x" @= int 3 .+ int 4
---    , "y" @= Var "x" .* int 6
---    , "z" @= neg $ max' (Var "x") (Var "y")
---    ]
---stat2 = Compound
---    [ "r" @= int 1
---    , "i" @= int 0
---    , While (Var "i" .< Var "n") $ Compound
---        [ "i" @= Var "i" .+ int 1
---        , "r" @= Var "r" .* Var "i"
---        ]
---    ]
+max' x y = If (x .< y) y x
+expr1 = Var "x" .+ int 3
+expr2 = If (Var "x") (Var "y" .- int 3) (int 2)
+stat1 = Compound
+    [ "x" @= int 3 .+ int 4
+    , "y" @= Var "x" .* int 6
+    , "z" @= neg $ max' (Var "x") (Var "y")
+    ]
+stat2 = Compound
+    [ "r" @= int 1
+    , "i" @= int 0
+    , While (Var "i" .< Var "n") $ Compound
+        [ "i" @= Var "i" .+ int 1
+        , "r" @= Var "r" .* Var "i"
+        ]
+    ]
 
---testsExpr = [ errorsCount (evalExpr M.empty expr1) ~?= 1
---            , evalExpr (M.fromList [("x", B True), ("y", I 5)]) expr2 ~?= Right (I 2)
---            , evalExpr (M.fromList [("x", B False), ("y", B False)]) expr2 ~?= Right (I 2)
---            , errorsCount (evalExpr (M.fromList [("x", B True), ("y", B False)]) expr2) ~?= 1
---            , fmap (M.lookup "z") (evalStatement M.empty stat1) ~?= Right (Just $ I $ -42)
---            , fmap (M.lookup "r") (evalStatement (M.fromList [("n", I 6)]) stat2) ~?= Right (Just $ I 720)
---            ]
---  where errorsCount = either length (const 0)
+testsExpr = [ errorsCount (evalExpr M.empty expr1) ~?= 1
+            , evalExpr (M.fromList [("x", B True), ("y", I 5)]) expr2 ~?= Right (I 2)
+            , evalExpr (M.fromList [("x", B False), ("y", B False)]) expr2 ~?= Right (I 2)
+            , errorsCount (evalExpr (M.fromList [("x", B True), ("y", B False)]) expr2) ~?= 1
+            , fmap (M.lookup "z") (evalStatement M.empty stat1) ~?= Right (Just $ I $ -42)
+            , fmap (M.lookup "r") (evalStatement (M.fromList [("n", I 6)]) stat2) ~?= Right (Just $ I 720)
+            ]
+  where errorsCount = either length (const 0)
 
 --------------------------------------------------------------------------------
 ---- 4. Реализовать двоичное дерево поиска без балансировки.
@@ -187,36 +202,38 @@ lookup sk (Branch k' v l r) | k' == sk = Just v
                             | k' > sk = lookup sk l
                             | k' < sk = lookup sk r
 
---insert :: Ord k => k -> v -> Map k v -> (Map k v, Maybe v)
---insert = k Leaf =  
+insert :: Ord k => k -> v -> Map k v -> (Map k v, Maybe v)
+insert = undefined -- k Leaf =  
 
---delete :: Ord k => k -> Map k v -> Maybe (Map k v)
---delete k Leaf = Nothing
---delete sk (Branch k' v l r) | k' == sk...
+delete :: Ord k => k -> Map k v -> Maybe (Map k v)
+delete = undefined
+{-
+delete k Leaf = Nothing
+delete sk (Branch k' v l r) | k' == sk...
+-}
 
+fromList :: Ord k => [(k, v)] -> Map k v
+fromList = undefined
 
---fromList :: Ord k => [(k, v)] -> Map k v
---fromList = undefined
-
---toList :: Map k v -> [(k, v)]
---toList = undefined
+toList :: Map k v -> [(k, v)]
+toList = undefined
 
 ---- tests
 
---sort :: Ord a => [a] -> [a]
---sort = map fst . toList . fromList . map (\x -> (x, ()))
+sort :: Ord a => [a] -> [a]
+sort = map fst . toList . fromList . map (\x -> (x, ()))
 
 --------------------------------------------------------------------------------
 -- main
 
---main = fmap (\_ -> ()) $ runTestTT $ test
---      $  label "complex" testsComplex
---      ++ label "height" testsHeight
---      ++ label "avg" testsAvg
---      ++ label "width" testsWidth
---      ++ label "Expr" testsExpr
---      ++ label "Map" -- можете сами написать тесты на каждую функцию :)
---            [ sort [10,24,13,56,35,13,6,23] ~?= [6,10,13,23,24,35,56] ]
---  where
---    label :: String -> [Test] -> [Test]
---    label l = map (\(i,t) -> TestLabel (l ++ " [" ++ show i ++ "]") t) . zip [1..]
+main = fmap (\_ -> ()) $ runTestTT $ test
+      $  label "complex" testsComplex
+      ++ label "height" testsHeight
+      ++ label "avg" testsAvg
+      ++ label "width" testsWidth
+      ++ label "Expr" testsExpr
+      ++ label "Map" -- можете сами написать тесты на каждую функцию :)
+            [ sort [10,24,13,56,35,13,6,23] ~?= [6,10,13,23,24,35,56] ]
+  where
+    label :: String -> [Test] -> [Test]
+    label l = map (\(i,t) -> TestLabel (l ++ " [" ++ show i ++ "]") t) . zip [1..]
