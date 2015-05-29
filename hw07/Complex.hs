430b1846679ae944c7504ed14784ecb1b5f142ce
RaiaN <raianknight@gmail.com>
2015-05-29 13:23:22 +0300

    Add part of hw07

--- hw07/Complex.hs
+++ hw07/Complex.hs
@@ -5,24 +5,42 @@ module Complex where
 data Complex = Complex { real :: Double, im :: Double } deriving Eq
 
 instance Num Complex where
-    (+) = undefined
-    (*) = undefined
-    fromInteger = undefined
-    negate = undefined
-    abs = undefined
+    (+) a b = Complex (real a + real b) (im a + im b)    
+    (*) a b = Complex (real a * real b - im a * im b) (real a * im b + im a * real b) 
+    fromInteger i = Complex (fromIntegral i :: Double) (0.0 :: Double)
+    negate a = Complex (-(real a)) (-(im a))
+    abs a = Complex (sqrt $ (real a * real a + im a * im a)) 0  
     
     signum _ = error "Complex: signum isn't defined"
 
 instance Fractional Complex where
-    (/) = undefined
-    fromRational = undefined
+    (/) (Complex a b) (Complex c d) = Complex ((a*c + b*d)/(c*c+d*d)) ((b*c-a*d)/(c*c+d*d))     
+    fromRational a = Complex (fromRational a) 0
 
 -- show и read должны работать как описано в тестах в Main.hs
+
 instance Show Complex where
-    show = undefined
+    show (Complex 0 0) = "0"
+    show (Complex a 0) = show a
+    show (Complex 0 (-1)) = "-i"
+    show (Complex 0 1) = "i"
+    show (Complex 0 b) = show b ++ " * i"
+    show (Complex a (-1)) = show a ++ " - i"
+    show (Complex a 1) = show a ++ " + i"             
+    show (Complex a b) | b > 0 = show a ++ " + " ++ show b ++ " * i"
+                       | b < 0 = show a ++ " - " ++ show (negate $ Complex 0 b)
+
+read' :: [String] -> [(Complex, String)]
+read' [rp] = [(Complex (read rp :: Double) 0, "")]
+read' [im, "*", "i"] = [(Complex 0 (read im :: Double), "")]
+read' [rp, "+", "i"] = [(Complex (read rp :: Double) 1, "")]
+read' [rp, "-", "i"] = [(Complex (read rp :: Double) (-1), "")]
+read' [rp,"+",im,r1,r2] = [(Complex (read rp :: Double) (read im :: Double), "")]
+read' [rp,"-",im,r1,r2] = [(Complex (read rp :: Double) (-(read im :: Double)), "")]
 
 instance Read Complex where
-    readsPrec = undefined
+    readsPrec _ s = read' $ words s
+
 
 i :: Complex
-i = undefined
+i = Complex 0 1 
--- hw07/Tree.hs
+++ hw07/Tree.hs
@@ -1,15 +1,18 @@
 module Tree where
 
+import Data.List	
+
 -- (2 балла)
 
 data Tree a = Node { value :: a, children :: [Tree a] } deriving Eq
 
 -- show и read должны работать как описано в тестах в Main.hs
 instance Show a => Show (Tree a) where
-    show = undefined
+    show (Node v []) = show v 
+    show (Node v xs) = show v ++ ":{" ++ (intercalate "," (map show xs)) ++ "}"
 
 instance Read a => Read (Tree a) where
     readsPrec = undefined
 
 instance Functor Tree where
-    fmap = undefined
+    fmap f (Node v xs) = Node (f v) $ map (fmap f) xs
